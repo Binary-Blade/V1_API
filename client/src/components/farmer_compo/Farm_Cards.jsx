f5871dd6a38a21cd89789b031b577dbbfff3bee5
R_evelare <houvillev@gmail.com>
2023-07-01 23:25:58 +0200

    Refactoring Farm_cards

--- client/src/components/farmer_compo/Farm_Cards.jsx
+++ client/src/components/farmer_compo/Farm_Cards.jsx
@@ -1,109 +1,141 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Card from '@mui/material/Card';
-import CardHeader from '@mui/material/CardHeader';
-import CardMedia from '@mui/material/CardMedia';
-import CardContent from '@mui/material/CardContent';
-import CardActions from '@mui/material/CardActions';
-import Collapse from '@mui/material/Collapse';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
-import FavoriteIcon from '@mui/icons-material/Favorite';
-import ShareIcon from '@mui/icons-material/Share';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import MoreVertIcon from '@mui/icons-material/MoreVert';
-import theme from '../../ui/theme';
+import React from 'react';
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  Button,
+  Card,
+  CardActionArea,
+  CardContent,
+  Tooltip,
+} from '@mui/material';
+import { styled } from '@mui/system';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import LabelIcon from '@mui/icons-material/Label';
 
-const ExpandMore = styled((props) => {
-  const { expand, ...other } = props;
-  return <IconButton {...other} />;
-})(({ theme, expand }) => ({
-  transform: !expand ? 'rotate(0deg)' : 'rotate(180deg)',
-  marginLeft: 'auto',
-  transition: theme.transitions.create('transform', {
-    duration: theme.transitions.duration.shortest,
-  }),
+const ImageBox = styled(Box)(({ theme }) => ({
+  position: 'relative',
+  width: '100%',
+  height: 0,
+  paddingBottom: '55%',
+  '@media (min-width:600px)': {
+    paddingBottom: '60%',
+  },
+  '@media (min-width:960px)': {
+    paddingBottom: '75%',
+  },
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundColor: '#fafafa',
+  borderRadius: '10px 10px 0 0',
 }));
 
-export default function FarmsCard() {
-  const [expanded, setExpanded] = React.useState(false);
+const StyledCard = styled(Card)(({ theme }) => ({
+  position: 'relative',
+  height: '100%',
+  display: 'flex',
+  flexDirection: 'column',
+  '&:hover': {
+    transform: 'scale(1.05)',
+    transition: 'transform .3s',
+  },
+}));
 
-  const handleExpandClick = () => {
-    setExpanded(!expanded);
-  };
+const Description = styled(Typography)(({ theme }) => ({
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  color: '#fff',
+  textAlign: 'center',
+  padding: theme.spacing(2),
+  backgroundColor: 'rgba(0, 0, 0, 0.7)',
+  borderRadius: theme.spacing(1),
+  display: 'none',
+  [theme.breakpoints.up('sm')]: {
+    display: 'block',
+  },
+}));
 
+const LabelContainer = styled(Box)(({ theme }) => ({
+  position: 'absolute',
+  top: 10,
+  right: 10,
+  display: 'flex',
+  flexDirection: 'row',
+}));
+
+const farmers = [
+  {
+    id: 1,
+    name: 'Farm 1',
+    location: 'Location 1',
+    description: 'Description 1',
+    labels: ['Label Rouge', 'Bio'],
+    imagePath: '/path-to-your-image-1.png',
+  },
+  {
+    id: 2,
+    name: 'Farm 2',
+    location: 'Location 2',
+    description: 'Description 2',
+    labels: ['Label Rouge'],
+    imagePath: '/path-to-your-image-2.png',
+  },
+  {
+    id: 3,
+    name: 'Farm 3',
+    location: 'Location 3',
+    description: 'Description 3',
+    labels: ['Bio'],
+    imagePath: '/path-to-your-image-3.png',
+  },
+  // You can add more farms here...
+];
+
+const FarmerPage = () => {
   return (
-    <Card
-      sx={{
-        maxWidth: 500,
-        bgcolor: theme.palette.background.secondary,
-        backgroundSize: 'center',
-      }}
-    >
-      <CardHeader
-        action={
-          <IconButton aria-label="settings">
-            <MoreVertIcon />
-          </IconButton>
-        }
-        title="Shrimp and Chorizo Paella"
-        subheader="September 14, 2016"
-        titleTypographyProps={{
-          variant: 'h4',
-          color: theme.palette.title.primary,
-        }}
-        subheaderTypographyProps={{
-          variant: 'body',
-          color: theme.palette.title.primary,
-        }}
-      />
-      <CardMedia
-        component="img"
-        height="194"
-        image="https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1378&q=80"
-        alt="Paella dish"
-        style={{ objectFit: 'contain', width: '100%' }}
-      />
-      <CardContent>
-        <Typography
-          variant="body"
-          color={theme.palette.title.primary}
-          sx={{ fontSize: 12 }}
-        >
-          This impressive paella is a perfect party dish and a fun meal to cook
-          together with your guests. Add 1 cup of frozen peas along with the
-          mussels, if you like.
-        </Typography>
-      </CardContent>
-      <CardActions disableSpacing>
-        <IconButton aria-label="add to favorites">
-          <FavoriteIcon />
-        </IconButton>
-        <IconButton aria-label="share">
-          <ShareIcon />
-        </IconButton>
-        <ExpandMore
-          expand={expanded}
-          onClick={handleExpandClick}
-          aria-expanded={expanded}
-          aria-label="show more"
-        >
-          <ExpandMoreIcon />
-        </ExpandMore>
-      </CardActions>
-      <Collapse in={expanded} timeout="auto" unmountOnExit>
-        <CardContent>
-          <Typography
-            paragraph
-            variant="body"
-            color={theme.palette.title.primary}
-            sx={{ fontSize: 12 }}
-          >
-            Method:
-          </Typography>
-          {/* More paragraphs */}
-        </CardContent>
-      </Collapse>
-    </Card>
+    <Container>
+      <Grid container spacing={3} justifyContent="center">
+        {farmers.map((farmer) => (
+          <Grid item xs={12} sm={6} md={4} key={farmer.id}>
+            <StyledCard>
+              <CardActionArea
+                sx={{ flexGrow: 1, display: 'flex', flexDirection: 'column' }}
+              >
+                <ImageBox sx={{ backgroundImage: `url(${farmer.imagePath})` }}>
+                  <LabelContainer>
+                    {farmer.labels.map((label, index) => (
+                      <Tooltip title={label} key={index}>
+                        <LabelIcon color="primary" sx={{ mx: 0.5 }} />
+                      </Tooltip>
+                    ))}
+                  </LabelContainer>
+                  <Description>{farmer.description}</Description>
+                </ImageBox>
+                <CardContent sx={{ flexGrow: 1 }}>
+                  <Typography variant="h6" gutterBottom>
+                    {farmer.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+              <Box
+                sx={{ p: 2, display: 'flex', justifyContent: 'center', gap: 2 }}
+              >
+                <Button variant="contained" color="primary">
+                  View Products
+                </Button>
+                <Button variant="contained" color="secondary">
+                  View Farm
+                </Button>
+              </Box>
+            </StyledCard>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
-}
+};
+
+export default FarmerPage;
