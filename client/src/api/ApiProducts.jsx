ed7dca39a95432a083f5f100e0c8662d726227dc
R_evelare <houvillev@gmail.com>
2023-07-02 19:28:17 +0200

    Modify API

--- client/src/api/ApiProducts.jsx
+++ client/src/api/ApiProducts.jsx
@@ -1,7 +1,10 @@
 import axios from 'axios';
 
-export function getProducts() {
-  return axios.get('http://127.0.0.1:8000/api_v1/products')
-    .then(res => res.data.data)
-    .catch(err => console.error(err));
+export async function getProducts() {
+  try {
+    const res = await axios.get('http://127.0.0.1:8000/api_v1/products');
+    return res.data.data;
+  } catch (err) {
+    return console.error(err);
+  }
 }
