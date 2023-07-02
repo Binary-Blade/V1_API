136df7e7058cec25179ac73743c2932efdb2976a
R_evelare <houvillev@gmail.com>
2023-07-02 19:28:45 +0200

    add pages routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,9 +4,14 @@ import { ThemeProvider } from '@mui/material/styles';
 import theme from './ui/theme';
 import ProductFarmer from './pages/AllProductsFarmer';
 import ProductPage from './pages/ProductPage';
-import getProduct from './pages/ProductPage';
+import ProductDetail from './pages/ProductDetail';
 import Homepage from './pages/Homepage';
+import Signup from './pages/Signup';
+import Login from './pages/Login';
 import MyCards from './pages/MyCards';
+import Validation from './components/command_compo/comandvalidation';
+import Payment from './pages/Payment';
+import OrderDetails from './components/command_compo/OrderDetail';
 import Contact from './pages/Contact';
 import Farmers from './pages/Farmers';
 import Checkout from './pages/Checkout';
@@ -21,12 +26,17 @@ export default function MyApp() {
       <ThemeProvider theme={theme}>
         <BrowserRouter>
           <Routes>
+            <Route path="signup" element={<Signup />} />
+            <Route path="login" element={<Login />} />
             <Route path="homepage" element={<Homepage />} />
             <Route path="products" element={<ProductFarmer />} />
             <Route path="farmers/product" element={<ProductPage />} />
-            <Route path="farmers/product/getProduct" element={<ProductPage />} />
+            <Route path="farmers/product/:id" element={<ProductDetail />} />
             <Route path="cards" element={<MyCards />} />
             <Route path="checkout" element={<Checkout />} />
+            <Route path="payment" element={<Payment />} />
+            <Route path="validation" element={<Validation />} />
+            <Route path="orderdetails" element={<OrderDetails />} />
             <Route path="contact" element={<Contact />} />
             <Route path="farmers" element={<Farmers />} />
           </Routes>
